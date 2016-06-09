804fb07ac0b6e003d605adea4af71d1de2bf5519
Duy Nguyen <justin@Duys-Air.lan>
2016-06-09 19:06:13 +0200

    exercise 1 done

--- Exercise1/createA.m
+++ Exercise1/createA.m
@@ -1,11 +1,11 @@
-function A = createA(dim , data)  
-    A = zeros(dim, 6*3+1);
+function A = createA(dim , data, max_p)  
+    A = zeros(dim, max_p*3+1);
     A(:,1) = ones(dim, 1);
     v = data(1,:);
     w = data(2,:);
-    for p = 0 : 5
-        A(:, 3 * p + 1) = v.^(p+1);
-        A(:, 3 * p + 2) = w.^(p+1);
-        A(:, 3 * p + 3) = (v.*w).^(p+1);
+    for p = 1 : max_p
+        A(:, 3 * (p-1) + 2) = v.^p;
+        A(:, 3 * (p-1) + 3) = w.^p;
+        A(:, 3 * (p-1) + 4) = (v.*w).^p;
     end
 end
